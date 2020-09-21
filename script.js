2d2270717d32368e5c2e76cc1a11c5b452165b5c
Aurelie Castro <a.castro@interface3.be>
2020-09-21 12:33:56 +0200

    added index, style, script and media

---
+++ script.js
@@ -0,0 +1,14 @@
+var main = document.querySelector("main");
+
+var article = {
+    titre = "reouverture de l'école",
+    date = "19/08/20",
+};
+
+main.innerHTML += `
+<article>
+     <h2> " + titre + " </h2>
+     <h3>" + tire + " </h3>
+</article>
+` + main.innerHTML;
+
